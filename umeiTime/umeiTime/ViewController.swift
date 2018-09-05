78db318d5ece97c38a4736a4d5447e34b525fc6f
tiyunjia <yunjia.ti@holley.cn>
2018-09-05 16:04:00 +0800

    初始化项目

---
+++ umeiTime/umeiTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  umeiTime
+//
+//  Created by 提运佳 on 2018/9/5.
+//  Copyright © 2018年 提运佳. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
